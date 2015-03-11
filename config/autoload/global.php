3ef27fe86de67f98c57fbeec1380d5e950196546
jsundquist <jsundquist@gmail.com>
2015-03-10 20:24:48 -0500

    Changing the name of the database

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 return array(
     'db' => array(
         'driver'         => 'Pdo',
-        'dsn'            => 'mysql:dbname=giveaway;host=localhost',
+        'dsn'            => 'mysql:dbname=giveaways;host=localhost',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
