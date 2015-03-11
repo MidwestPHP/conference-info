b32294c3d340618a552991d6e8bfd37c952f192b
jsundquist <jsundquist@gmail.com>
2015-03-10 22:02:07 -0500

    Modified the setup sql file to include the creation of the database

--- config/setup.sql
+++ config/setup.sql
@@ -1,10 +1,12 @@
-Create Table prizes (
+CREATE DATABASE IF NOT EXISTS giveaways;
+
+Create Table giveaways.prizes (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR (200),
   available INT(1) DEFAULT 1
 );
 
-CREATE TABLE phonenumbers (
+CREATE TABLE giveaways.phonenumbers (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   number VARCHAR (200),
   prizeId INT(6),
